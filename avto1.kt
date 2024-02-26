8ccb9b1b007eaec524fcda1b50f6577853ac6a30
boryaxray <157354076+boryaxray@users.noreply.github.com>
2024-02-26 09:31:26 +0500

    Delete avto1.kt

--- avto1.kt
+++
@@ -1,8 +0,0 @@
-fun main(){
-    try {
-        val avt=avto(manufacture = "")
-        avt.Input(avto=avto(manufacture = ""))
-        if (avt.hp>0)avt.Output()
-        else avt.Output1()
-    }catch (e:Exception){ println("Неверный ввод")}
-}
